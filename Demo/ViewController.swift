c1b0bf86212432a8bb22e4e2c7363823ddbe2e9d
Christopher Ras <chris@MacBook-Pro.local>
2016-11-05 01:18:17 +0100

    Added functionality to the Input field and buttons

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var inputName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func displayName(_ sender: Any) {
+        name.text = inputName.text
+    }
 
 }
 
